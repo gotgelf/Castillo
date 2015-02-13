a493cd228879b750534ce1edb9fe7a149762937a
alex gotgelf <gotgelf@gmail.com>
2015-02-13 16:20:56 +0200

    Change some setup files

--- project/CatilloBuild.scala
+++ project/CatilloBuild.scala
@@ -3,20 +3,51 @@ import play._
 import sbt._
 import Keys._
 
-object CatilloBuild extends Build with UniversalKeys {
+object CastilloBuild extends Build with UniversalKeys {
+
+  val appName = "castillo"
 
   /**
-   * Define the root
+   * Define the root project as an aggregation of the sub projects.
    */
   lazy val root =
     project.in(file("."))
     .settings(commonSettings:_*)
+    .aggregate(server)
+
+  /**
+   * Define the server project as a Play application.
+   */
+  lazy val server =
+    project.in(file("server"))
+    .settings(serverSettings:_*)
     .enablePlugins(PlayScala)
 
+  /**
+   * The settings for the server module.
+   */
+  lazy val serverSettings = commonSettings ++ Seq(
+    name := s"$appname-server",
+    libraryDependencies ++= Dependencies.serverDeps
+  )
+
+  /**
+   * The setting that will be applied to all sub projects.
+   */
   lazy val commonSettings = Seq(
     organization := "de.woq",
     version := "1.0-SNAPSHOT",
     name := "castillo",
     scalaVersion := "2.11.4"
   )
+
+  /**
+   * A simple container for the dependencies.
+   */
+  object Dependencies {
+
+    lazy val serverDeps = Seq(
+      "com.newrelic.agent.java" % "newrelic-agent" % Versions.newRelic
+    )
+  }
 }
